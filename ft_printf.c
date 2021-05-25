26a38b43c12521dc4232d270a507777634442d9a
Pedro henrique Bielik <pbielik@c1r10s1.42lisboa.com>
2021-05-25 19:09:29 +0100

    first commit

---
+++ ft_printf.c
@@ -0,0 +1,293 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pbielik <pbielik@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/25 13:54:33 by pbielik           #+#    #+#             */
+/*   Updated: 2021/05/25 17:49:46 by pbielik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CONVERSIONS "cspdiuxX%"
+
+typedef struct s_flags {
+	int leftJustify;
+	int leftPads;
+	int precision;
+	int width;
+	int count;
+	int types;
+} t_flags;
+
+int		ft_printf(char *format, ...);
+void	ft_putchar(char c, t_flags *flags);
+int		parseFlags(t_flags *flags, char *format, va_list args, int i);
+int		ft_isdigit(int c);
+char	*ft_strchr(const char *str, int c);
+void	printType(t_flags *flags, va_list args);
+void	printWidth(t_flags *flags, int spaces, int width, int zero);
+size_t	ft_strlen(const char *str);
+void 	printPrecision(t_flags *flags, int len, char *str);
+char	*ft_itoa(int n);
+void 	printInt(t_flags *flags, int num);
+
+int main()
+{
+	ft_printf("%-010.2d", -14568);
+	ft_printf("%-30.8d", 14568.56);
+}
+
+int	ft_printf(char *format, ...)
+{
+	va_list args;
+	//t_flags flags;
+	va_start(args, format);
+	int i;
+
+	t_flags flags = {0, 0, -1, 0, 0, 0};
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			i = parseFlags(&flags, format, args, i);
+			printType(&flags, args);
+		}
+		else
+		{
+			ft_putchar(format[i], &flags);
+		}
+		i++;
+	}
+ 	printf("\n\nWidth: %d\n", flags.width);
+	printf("Precision: %d\n", flags.precision);
+	printf("Types: %c\n", flags.types);
+	printf("Left Pads: %d\n", flags.leftPads);
+	printf("Left Justify: %d\n", flags.leftJustify);
+	printf("Return: %d\n\n", flags.count);
+	return (flags.count);
+}
+
+int parseFlags(t_flags *flags, char *format, va_list args, int i)
+{
+	while (format[i])
+	{
+		if (format[i] == '-') {
+			flags->leftJustify = 1;
+			flags->leftPads = 0;
+		}
+		if (format[i] == '0' && flags->leftJustify == 0 && flags->width == 0)
+			flags->leftPads = 1;
+		if (format[i] == '*'){
+			flags->width = va_arg(args, int);
+			if (flags->width < 0)
+			{
+				flags->width = flags->width * -1;
+				flags->leftJustify = 1;
+			}
+		}
+		if (format[i] == '.'){
+			flags->precision = 0;
+			i++;
+			if (format[i] == '*') {
+				flags->precision = va_arg(args, int);
+			}
+			while (ft_isdigit(format[i]))
+			{
+				flags->precision = flags->precision * 10 + format[i] - 48;
+				i++;
+			}
+		}
+		if (ft_isdigit(format[i]))
+			flags->width = flags->width * 10 + format[i] - 48;
+		if (ft_strchr(CONVERSIONS, format[i]))
+		{
+			flags->types = format[i];
+			break;
+		}
+		i++;
+	}
+	return (i);
+}
+
+void	printType(t_flags *flags, va_list args)
+{
+	if (flags->types == 'c')
+	{
+		if (!flags->leftJustify)
+			printWidth(flags, 1, 0, 0);
+
+		ft_putchar(va_arg(args, int), flags);
+
+		if (flags->leftJustify)
+			printWidth(flags, 1, 0, 0);
+	}
+
+	if (flags->types == 's')
+	{
+		char *str = va_arg(args, char *);
+		if (str == NULL)
+			str = "(null)";
+		if (flags->precision > ft_strlen(str) || flags->precision <= -1)
+			flags->precision = ft_strlen(str);
+
+		if (!flags->leftJustify)
+			printWidth(flags, flags->precision, flags->width, 0);
+
+		printPrecision(flags, flags->precision, str);
+
+		if (flags->leftJustify)
+			printWidth(flags, flags->precision, flags->width, 0);
+	}
+
+	if (flags->types == 'd' || flags->types == 'i')
+	{
+		printInt(flags, va_arg(args, int));
+
+	}
+}
+
+void printInt(t_flags *flags, int num)
+{
+	char *str = ft_itoa(num);
+	int neg = 0;
+
+	if (num < 0)
+		neg = 1;
+
+	if (flags->leftPads && flags->precision == -1)
+	{
+		printWidth(flags, ft_strlen(str), flags->width - neg, 1);
+		if (neg)
+			ft_putchar('-', flags);
+		printPrecision(flags, ft_strlen(str), str);
+	}
+	else if (!flags->leftJustify)
+	{
+		if (flags->precision > ft_strlen(str))
+			printWidth(flags, flags->precision, flags->width - neg, 0);
+		else
+			printWidth(flags, ft_strlen(str), flags->width - neg, 0);
+		if (neg)
+			ft_putchar('-', flags);
+		printWidth(flags, ft_strlen(str), flags->precision, 1);
+		printPrecision(flags, ft_strlen(str), str);
+	}
+	else if (flags->leftJustify)
+	{
+		if (neg)
+			ft_putchar('-', flags);
+		printWidth(flags, ft_strlen(str), flags->precision, 1);
+		printPrecision(flags, ft_strlen(str), str);
+		if (flags->precision > ft_strlen(str))
+			printWidth(flags, flags->precision, flags->width - neg, 0);
+		else
+			printWidth(flags, ft_strlen(str), flags->width - neg, 0);
+	}
+
+}
+
+void	printWidth(t_flags *flags, int spaces, int width, int zero)
+{
+	int size;
+
+	size = width - spaces;
+	while (size > 0){
+		if (zero)
+			ft_putchar('0', flags);
+		else
+			ft_putchar('.', flags);
+		size--;
+	}
+}
+
+void printPrecision(t_flags *flags, int len, char *str)
+{
+	int i;
+
+	i = 0;
+	while (i < len) {
+		ft_putchar(str[i], flags);
+		i++;
+	}
+}
+
+void	ft_putchar(char c, t_flags *flags)
+{
+	write(1, &c, 1);
+	flags->count += 1;
+}
+
+int	ft_isdigit(int c)
+{
+	if ((c >= '0' && c <= '9'))
+		return (1);
+	return (0);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	while (*str)
+	{
+		if (*str == (char)c)
+			return ((char *)str);
+		str++;
+	}
+	if ((char)c == '\0')
+		return ((char *)str);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int		ft_counter(int n)
+{
+	size_t	counter;
+
+	counter = 0;
+	if (n == 0)
+		return (1);
+	while (n > 0)
+	{
+		n /= 10;
+		counter++;
+	}
+	return (counter);
+}
+
+char	*ft_itoa(int n)
+{
+	size_t	len;
+	char	*tab;
+
+	if (n < 0)
+		n *= -1;
+	if (n == 0)
+		return ("0");
+	len = ft_counter(n);
+	if (!(tab = (char *)malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	tab[len] = '\0';
+	while (len > 0)
+	{
+		tab[--len] = n % 10 + 48;
+		n /= 10;
+	}
+	return (tab);
+}
